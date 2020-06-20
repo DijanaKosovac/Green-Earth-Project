1b9075ee18d5196718a6af8f8d1b9073487030cb
DijanaKosovac <kosovacdijana@gmail.com>
2020-06-20 18:10:36 +0200

    Code cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,43 @@
-// const gulp = require('gulp');
-// const imagemin = require('gulp-imagemin');
-// const concat = require('gulp-concat');
-// const terser = require('gulp-terser');
-// const sourcemaps = require('gulp-sourcemaps');
-// const postcss = require('gulp-postcss');
-// const cssnano = require('cssnano');
-// const autoprefixer = require('autoprefixer');
-// const sass = require('gulp-sass');
-// const browserSync = require('browser-sync').create();
-// const { src, series, parallel, dest, watch } = require('gulp');
-
-// const jsPath = 'src/js/**/*.js';
-// const cssPath = 'src/css/**/*.css';
-// const sassPath = 'src/scss/**/*.scss';
-
-
-// function copyHtml() {
-//     return src('src/*.html').pipe(gulp.dest('dist'));
-// }
-
-// function imgTask() {
-//     return src('src/images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'));
-// }
-
-// function jsTask() {
-//     return src(jsPath)
-//         .pipe(sourcemaps.init())
-//         .pipe(concat('all.js'))
-//         .pipe(terser())
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(dest('dist/assets/js'));
-// }
-
-// function cssTask() {
-//     return src(cssPath)
-//         .pipe(sourcemaps.init())
-//         .pipe(concat('style.css'))
-//         .pipe(postcss([autoprefixer(), cssnano()]))
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(dest('dist/assets/css'));
-// }
-
-
-// ONLY FOR DEVELOPMENT
-// function sassTask() {
-//     return src(sassPath)
-//         .pipe(sass())
-//         .pipe(dest('src/css'))
-//         .pipe(browserSync.stream());
-// }
-
-
-// function watchTask() {
-//     watch([cssPath, jsPath, sassPath], { interval: 1000 }, sassTask);
-// }
-
-// function watch() {
-//     browserSync.init({
-//         server: {
-//             baseDir: './'
-//         }
-//     });
-//     gulp.watch(sassPath, sassTask);
-//     gulp.watch('src/*.html').on('change', browserSync.reload);
-//     gulp.watch(jsPath, jsTask).on('change', browserSync.reload);
-// }
-
-// exports.sassTask = sassTask;
-
-// exports.jsTask = jsTask;
-// exports.imgTask = imgTask;
-// exports.default = series(
-//     parallel(copyHtml, imgTask, sassTask),
-//     watchTask
-// );
-// exports.watch = watch;
-
-
-// new gulp
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const { src, parallel, dest } = require('gulp');
+
+const jsPath = 'src/js/**/*.js';
+const cssPath = 'src/css/**/*.css';
+
+function copyHtml() {
+    return src('src/*.html').pipe(gulp.dest('dist'));
+}
+
+function imgTask() {
+    return src('src/images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'));
+}
+
+function jsTask() {
+    return src(jsPath)
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist/js'));
+}
+
+function cssTask() {
+    return src(cssPath)
+        .pipe(sourcemaps.init())
+        .pipe(concat('style.css'))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist/css'));
+}
 
 
 function style() {
@@ -101,5 +58,5 @@ function watch() {
     gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
 }
 
-exports.style = style;
 exports.watch = watch;
+exports.build = parallel(copyHtml, imgTask, jsTask, cssTask);
--- src/js/main.js
+++ src/js/main.js
@@ -1,78 +1,67 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
+let arrowLeft = document.querySelector("#prev"),
+    arrowRight = document.querySelector("#next"),
+    slider = document.getElementById('slider'),
+    current = 0;
 
-
-// Select tab content item
 function selectItem(e) {
-    // Remove all show and border classes
     removeBorder();
     removeShow();
-    // Add border to current tab item
     this.classList.add('tab-border');
-    // Grab content item from DOM
     const tabContentItem = document.querySelector(`#${this.id}-content`);
-    // Add show class
     tabContentItem.classList.add('show');
 }
 
-// Remove bottom borders from all tab items
 function removeBorder() {
     tabItems.forEach(item => {
         item.classList.remove('tab-border');
     });
 }
 
-// Remove show class from all content items
 function removeShow() {
     tabContentItems.forEach(item => {
         item.classList.remove('show');
     });
 }
 
-// Listen for tab item click
 tabItems.forEach(item => {
     item.addEventListener('click', selectItem);
 });
 
+function backgroundImageWithGradient(imageName) {
+    return `linear-gradient(rgba(0,0,0, 0.4),rgba(0,0,0,0.4)),url('images/carousel-${imageName}.png') `;
+}
 
 
-
-// slideshow
-
-
-
-let arrowLeft = document.querySelector("#prev"),
-    arrowRight = document.querySelector("#next"),
-    slider = document.getElementById('slider'),
-    current = 0;
-
-
-// Left arrow click
 arrowLeft.addEventListener("click", function () {
     if (current === 0) {
         return;
     }
     current--;
     if (current === 1) {
-        slider.style.backgroundImage = `linear-gradient(rgba(0,0,0, 0.4),rgba(0,0,0,0.4)),url('images/carousel-img2.png') `;
+        slider.style.backgroundImage = backgroundImageWithGradient('img2');
     } else {
-        slider.style.backgroundImage = `linear-gradient(rgba(0,0,0, 0.4),rgba(0,0,0,0.4)),url('images/carousel-img1.png') `;
+        slider.style.backgroundImage = backgroundImageWithGradient('img1');
     }
 });
 
-// Right arrow click
 arrowRight.addEventListener("click", function () {
     if (current === 2) {
-        slider.style.backgroundImage = `linear-gradient(rgba(0,0,0, 0.4),rgba(0,0,0,0.4)),url('images/carousel-img3.png') `;
+        slider.style.backgroundImage = backgroundImageWithGradient('img3');
         return;
     }
     current++;
     if (current === 1) {
-        slider.style.backgroundImage = `linear-gradient(rgba(0,0,0, 0.4),rgba(0,0,0,0.4)),url('images/carousel-img2.png') `;
+        slider.style.backgroundImage = backgroundImageWithGradient('img2');
     } else {
-        slider.style.backgroundImage = `linear-gradient(rgba(0,0,0, 0.4),rgba(0,0,0,0.4)),url('images/carousel-img3.png') `;
+        slider.style.backgroundImage = backgroundImageWithGradient('img3');
     }
 });
+
+
+
+
 
 
 
